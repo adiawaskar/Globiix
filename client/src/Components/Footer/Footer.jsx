b169b3cbeb0ccf23c59f58ede7f10df5154490f8
Ritika Nankar <112073087+Ritika128@users.noreply.github.com>
2024-05-17 00:56:34 +0530

    added footer

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
+    <div className="footer-container">
+      <div className="column">
+        <h3>Contact Us : </h3>
+        <p>Email: example@example.com</p>
+        <p>Call Us: +91 4562738</p>
+        <p>Head Office: VJTI, Matunga</p>
+      </div>
+      <div className="column">
+        <h3>Services : </h3>
+        <ul>
+          <li>Trek Packages</li>
+          <li>Classic Tent Camping</li>
+          <li>Family Tent Camping</li>
+          <li>Special Offer Packages</li>
+        </ul>
+      </div>
+      <div className="column">
+        <h3>Newsletter : </h3>
+        <p>Subscribe for daily travel newsletter:</p>
+        <input type="email" placeholder="Your Email" />
+      </div>
+      
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
